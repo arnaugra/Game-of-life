86c10bccf9c4e255e803ec42655b17648f9db3ca
Arnau Granados <arnau.gra@gmail.com>
2022-09-28 01:41:34 +0200

    Function to change cell state

--- GOL.js
+++ GOL.js
@@ -7,11 +7,25 @@ function board() {
 
         for (let row = 0; row < HEIGHT; row++) {
             td = document.createElement("td");
+            td.setAttribute("onclick", "cellStatus(this)");
+            td.setAttribute("class", "dead");
             tr.append(td);
 
         }
 
-        TABLE?.appendChild(tr);
+        TABLE.appendChild(tr);
+    }
+}
+
+function cellStatus(box) {
+    if (box.classList.contains("dead")) {
+        box.classList.remove("dead");
+        box.classList.add("alive");
+        box.style.backgroundColor = "#000000";
+    } else {
+        box.classList.remove("alive");
+        box.classList.add("dead");
+        box.style.backgroundColor = "#eeeeee";
     }
 }
 
