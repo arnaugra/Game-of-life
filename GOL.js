c0cd8fa19e10e3bab25e833074e89d85441a2bda
Arnau Granados <arnau.gra@gmail.com>
2022-09-28 02:10:21 +0200

    Buttons Clear and Random doing their function properly

--- GOL.js
+++ GOL.js
@@ -1,6 +1,9 @@
 const HEIGHT = 25;
-let TABLE = document.querySelector("table");
+const TABLE = document.querySelector("table");
 
+/**
+ * Build the board, too lazy to do it, this is faster
+ */
 function board() {
     for (let col = 0; col < HEIGHT; col++) {
         tr = document.createElement("tr");
@@ -17,18 +20,50 @@ function board() {
     }
 }
 
-function cellStatus(box) {
-    if (box.classList.contains("dead")) {
-        box.classList.remove("dead");
-        box.classList.add("alive");
-        box.style.backgroundColor = "#000000";
+/**
+ * Change the status (dead/alive) of the cell indicated
+ * @param {HTMLTableCellElement} cell - Cell clicked 
+ */
+function cellStatus(cell) {
+    if (cell.classList.contains("dead")) {
+        cell.classList.remove("dead");
+        cell.classList.add("alive");
+        cell.style.backgroundColor = "#000000";
     } else {
-        box.classList.remove("alive");
-        box.classList.add("dead");
-        box.style.backgroundColor = "#eeeeee";
+        cell.classList.remove("alive");
+        cell.classList.add("dead");
+        cell.style.backgroundColor = "#eeeeee";
     }
 }
 
+/**
+ * Gets all the cells from the board and with a random number (1 to 3) decides the cell state (1 alive / 2-3 dead)
+ */
+function cellStatusRandom() {
+    clearCell();
+    let CELLS = document.querySelectorAll("td");
+    CELLS.forEach(cell => {
+        if (Math.ceil(Math.random() * 3) == 1) {
+            cellStatus(cell);
+        }
+    });
+}
+
+/**
+ * Clears the board of alive cells, all of them become dead
+ */
+function clearCell() {
+    let CELLS = document.querySelectorAll("td");
+    CELLS.forEach(cell => {
+        cell.classList.remove("alive");
+        cell.classList.add("dead");
+        cell.style.backgroundColor = "#eeeeee";
+    });
+}
+
+/**
+ * Execute the board building when the page loads
+ */
 window.onload = () => {
     board();
 }
